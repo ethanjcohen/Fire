797eddc16a822f4d240841d21d5181e80284e2ba
Ethan Cohen <ethan.cohen@veepworks.com>
2016-10-21 18:09:48 -0500

    Added support for AES encryption

--- example.js
+++ example.js
@@ -6,6 +6,19 @@ configs.events.on('refreshed', function() {
     //notify your system that new configs are available
 })
 
+server.use(
+    function(req, res, next)
+    {
+        console.log('Input: ', req.body)
+        return next()
+    },
+    function(req, res, next)
+    {
+        console.log('Second middleware')
+        return next()
+    }
+)
+
 server.start(function(err) {
     if(!err) {
         //The REST API server is now running
@@ -13,4 +26,4 @@ server.start(function(err) {
         //Refresh the configs from our repo
         configs.refresh()
     }
-});
+})
